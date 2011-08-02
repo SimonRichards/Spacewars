67e224a94b35ab0be721c0e2c7b0bf8f8a8ceab6
scr52 <scr52@ea70c998-2c9d-0410-ad4e-af625dd3c7ea>
2011-08-02 23:25:16 +0000

    Some docs

--- src/common/Connection.java
+++ src/common/Connection.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- *
+ * Sub-classes of Connection abstract sending and receiving data to
+ * their respective endpoints, all important operations throw any
+ * IO Exceptions they cause so it is up to the user to handle these.
  * @author Simon, Daniel
  */
 public abstract class Connection {
@@ -24,24 +26,32 @@ public abstract class Connection {
     protected String name;
 
     /**
-     * @return The server's reported name
+     * @return The other end's reported name
      */
     public String getName() {
         return name;
     }
 
+    /**
+     * Connection.Client encapsulates the server's communication with a client
+     */
     public static class Client extends Connection {
         private static final int MAX_NAME_LENGTH = 20;
 
         private int id;
 
+        /**
+         * Creates a new client connection on the given socket
+         * @param socket The socket on which a client is already connected
+         * @throws IOException If the handshake data is not received correctly
+         */
         public Client(Socket socket) throws IOException {
             this.socket = socket;
             socket.setTcpNoDelay(true);
             out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
             in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             id = in.readInt();
-            
+
             char[] buffer = new char[MAX_NAME_LENGTH];
             int i = 0;
             do {
@@ -50,15 +60,19 @@ public abstract class Connection {
             name = String.copyValueOf(buffer);
         }
 
+        /**
+         * @return The client's unique identifier
+         */
         public int getID() {
             return id;
         }
 
         /**
-         *
-         * @param commandBuffer
+         * Fills a command buffer with the users input commands.
+         * @param commandBuffer an empty buffer to be filled with the ordinal
+         * values of the client's commands
          * @return The number of commands received
-         * @throws IOException
+         * @throws IOException If a connection problem occurs
          */
         public int getCommands(int[] commandBuffer) throws IOException {
             int i = 0;
@@ -72,6 +86,11 @@ public abstract class Connection {
             return i;
         }
 
+        /**
+         * Sends the client a list of identifiers for all current actors on this server
+         * @param actors The actors to convert into a header and transmit
+         * @throws IOException if transmission fails
+         */
         public void sendHeader(ArrayList<Actor> actors) throws IOException {
             out.writeInt(actors.size());
             for (Actor actor : actors) {
@@ -81,6 +100,11 @@ public abstract class Connection {
             out.flush();
         }
 
+        /**
+         * Takes an already marshaled actor and transmits
+         * @param actorBuffer The actor data to transmit
+         * @throws IOException if transmission fails
+         */
         public void sendActor(double[] actorBuffer) throws IOException {
             for (int i = 0; i < actorBuffer.length; i++) {
                 out.writeDouble(actorBuffer[i]);
@@ -88,6 +112,9 @@ public abstract class Connection {
             out.flush();
         }
 
+        /**
+         * Attempts to close the socket, failure not registered
+         */
         public void close() {
             try {
                 socket.close();
@@ -97,12 +124,25 @@ public abstract class Connection {
         }
     }
 
+
+    /**
+     * Encapsulates how a client sees a server and handles all data
+     * transmission and reception.
+     */
     public static class Server extends Connection {
 
         private final ArrayList<Integer> actorList;
         private long lastRefreshed;
         private static final long TIMEOUT = 1200;
 
+        /**
+         * Attempts a connection to the given machine and save's its name and id
+         * @param host The server's IP address
+         * @param port The port the server is listening on
+         * @param name The server's name
+         * @param id The client's id
+         * @throws IOException If the connection cannot be made
+         */
         public Server(InetAddress host, int port, String name, int id) throws IOException {
             this.name = name;
             actorList = new ArrayList<Integer>(Game.popcap);
@@ -124,14 +164,15 @@ public abstract class Connection {
          * @return true if this is the same server, false otherwise
          */
         public boolean is(InetAddress address, int port) {
-            //TODO: port is used as an ad-hoc server unique identifier as different techniques failed on different netrworks
+            //TODO: port is used as an ad-hoc server unique identifier as other
+            // techniques failed on different networks.
             if (socket.getPort() == port) {
                 return true;
             } else {
                 return false;
             }
             /*
-             * Here's the old code:
+             * Here's the old code which was previously used in addition to port matching:
              * socket.getLocalAddress().getHostAddress().equals(address.getHostAddress()) ||
              * (address.getHostAddress().equals(socket.getInetAddress().getHostAddress())
              */
@@ -207,15 +248,26 @@ public abstract class Connection {
             return Actor.ActorType.fromInt(actorList.get(2 * i + 1));
         }
 
+        /**
+         * Tests that a heartbeat has been received within the timeout period
+         * @return true if the server is still sending UDP packets
+         */
         public boolean isAlive() {
             return System.currentTimeMillis() - lastRefreshed < TIMEOUT;
         }
 
+        /**
+         * Notifies this object that a heartbeat was received from its remote host
+         */
         public void heartbeat() {
             lastRefreshed = System.currentTimeMillis();
         }
     }
 
+    /**
+     * Attempts to close the socket upon GC
+     * @throws Throwable a possible IOException
+     */
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
